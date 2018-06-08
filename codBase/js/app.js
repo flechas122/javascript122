b6851014da61dfc34a70f33b012227d5e182725b
sebastian lopez <slopezg59@gmail.com>
2018-06-08 16:55:53 -0500

    comienzo asignacion de teclas

---
+++ codBase/js/app.js
@@ -0,0 +1,70 @@
+var num1 = 0;
+var num2 = 0;
+var opera;
+
+
+document.getElementById("on").onclick = function() {
+  document.getElementById("display").textContent = "0";
+}
+
+/*
+document.getElementById("7").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "7";
+}
+
+document.getElementById("7").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "7";
+}
+*/
+
+/*
+document.getElementById("7").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "7";
+}
+*/
+
+document.getElementById("7").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "7";
+}
+document.getElementById("8").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "8";
+}
+document.getElementById("9").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "9";
+}
+
+/*
+document.getElementById("7").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "7";
+}
+*/
+
+document.getElementById("4").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "4";
+}
+document.getElementById("5").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "5";
+}
+document.getElementById("6").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "6";
+}
+/*
+document.getElementById("7").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "7";
+}
+*/
+document.getElementById("1").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "1";
+}
+document.getElementById("2").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "2";
+}
+document.getElementById("3").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "3";
+}
+
+/*
+document.getElementById("7").onclick = function() {
+  document.getElementById("display").textContent = display.textContent + "7";
+}
+*/
